e35401ff215cd2fba0f7b83428007b23f67ae8ac
Golikova Dasha <99117131+golikovaDasha@users.noreply.github.com>
2022-02-16 20:07:44 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -7,15 +7,14 @@ bool checkPrime(uint64_t value) {
 for (int i = 2; i < value; i++) {
    if (value % i == 0) {
      return false;
-   }
-
-   }
+  }
+  }
 return true;
 }
 int k = 2;
 uint64_t nPrime(uint64_t n) {
 while (n != 0) {
-   if (checkPrime(k) == true) {
+  if (checkPrime(k) == true) {
      k++;
      return nPrime(n - 1);
 }
@@ -30,14 +29,14 @@ while (1) {
 int c = 1;
 value++;
 for (int i = 2; i <= value; i++) {
-   if (value % i == 0) {
+  if (value % i == 0) {
      c++;
      }
-   }
+  }
 
 if (c == 2) {
-   return value;
-   }
+  return value;
+  }
   }
 }
 int m = 0;
@@ -46,14 +45,14 @@ while (hbound != 0) {
 int c = 1;
 hbound--;
 for (int i = 2; i <= hbound; i++) {
-   if (hbound % i == 0) {
+  if (hbound % i == 0) {
      c++;
      }
 }
 if (c == 2) {
-   m += hbound;
-   return sumPrime(hbound);
-   }
+  m += hbound;
+  return sumPrime(hbound);
+  }
 }
 return m;
 }
