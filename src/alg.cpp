97d7264b057902566007218a80e874b50cab5342
Golikova Dasha <99117131+golikovaDasha@users.noreply.github.com>
2022-02-16 17:32:21 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,53 +5,53 @@
 
 bool checkPrime(uint64_t value) {
 for (int i = 2; i < value; i++) {
-   if (value % i == 0) {
+if (value % i == 0) {
      return false;
-   }
-   }
+}
+}
     return true;
-   }
+}
 int k = 2;
 uint64_t nPrime(uint64_t n) {
 while (n != 0) {
-   if (prost(k) == true) {
-     k++;
+if (prost(k) == true) {
+	 k++;
      return noe(n - 1);
-   }
-   k++;
-   return noe(n);
-   }
-   return k-1;
+}
+k++;
+return noe(n);
+}
+return k-1;
 }
 
 uint64_t nextPrime(uint64_t value) {
 while (1) {
-   int c = 1;
-   value++;
-   for (int i = 2; i <= value; i++) {
-			if (value % i == 0) {
-				c++;
+int c = 1;
+value++;
+for (int i = 2; i <= value; i++) {
+  if (value % i == 0) {
+	 c++;
       }
-   }
+}
      if (c == 2) {
        return value;
     }
-	}
+  }
 }
 int m = 0;
 uint64_t sumPrime(uint64_t hbound) {
-while (hbound!=0) {
-   int c = 1;
-   hbound--;
-   for (int i = 2; i <= hbound; i++) {
-     if (hbound % i == 0) {
+while (hbound != 0) {
+int c = 1;
+hbound--;
+for (int i = 2; i <= hbound; i++) {
+	 if (hbound % i == 0) {
        c++;
      }
      }
-   if (c == 2) {
-     m += hbound;
+if (c == 2) {
+	 m += hbound;
      return sum(hbound);
-     }			
+}			
      }
-   return m;
+return m;
 }
